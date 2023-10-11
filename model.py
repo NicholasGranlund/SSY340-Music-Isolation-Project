1893b8a6d5b49b3482f9a4329119d2bf850c44c2
Martino1129 <martin.ducros@imt-atlantique.net>
2023-10-11 18:31:45 +0200

    fix

--- model.py
+++ model.py
@@ -4,24 +4,24 @@ from torch import nn
 
 class UnetModel(nn.Module):
 
-    def __init__(self, ngf=64, input_nc=1, output_nc=2):
+    def __init__(self, ngf=64, nc=1):
         super(UnetModel, self).__init__()
 
         # initialize layers
-        self.audionet_convlayer1 = self.unet_conv(input_nc, ngf)
+        self.audionet_convlayer1 = self.unet_conv(nc, ngf)
         self.audionet_convlayer2 = self.unet_conv(ngf, ngf * 2)
         self.audionet_convlayer3 = self.unet_conv(ngf * 2, ngf * 4)
         self.audionet_convlayer4 = self.unet_conv(ngf * 4, ngf * 8)
         self.audionet_convlayer5 = self.unet_conv(ngf * 8, ngf * 8)
         self.audionet_convlayer6 = self.unet_conv(ngf * 8, ngf * 8)
         self.audionet_convlayer7 = self.unet_conv(ngf * 8, ngf * 8)
-        self.upconvlayer1 = self.unet_upconv(ngf * 16, ngf * 8)
+        self.upconvlayer1 = self.unet_upconv(ngf * 8, ngf * 8)
         self.upconvlayer2 = self.unet_upconv(ngf * 16, ngf * 8)
         self.upconvlayer3 = self.unet_upconv(ngf * 16, ngf * 8)
         self.upconvlayer4 = self.unet_upconv(ngf * 16, ngf * 4)
         self.upconvlayer5 = self.unet_upconv(ngf * 8, ngf * 2)
         self.upconvlayer6 = self.unet_upconv(ngf * 4, ngf)
-        self.upconvlayer7 = self.unet_upconv(ngf * 2, output_nc, True)
+        self.upconvlayer7 = self.unet_upconv(ngf * 2, nc, True)
 
     def forward(self, x):
         audio_conv1feature = self.audionet_convlayer1(x)
@@ -31,13 +31,13 @@ class UnetModel(nn.Module):
         audio_conv5feature = self.audionet_convlayer5(audio_conv4feature)
         audio_conv6feature = self.audionet_convlayer6(audio_conv5feature)
         audio_conv7feature = self.audionet_convlayer7(audio_conv6feature)
-        audio_upconv1feature = self.audionet_upconvlayer1(audio_conv7feature)
-        audio_upconv2feature = self.audionet_upconvlayer2(torch.cat((audio_upconv1feature, audio_conv6feature), dim=1))
-        audio_upconv3feature = self.audionet_upconvlayer3(torch.cat((audio_upconv2feature, audio_conv5feature), dim=1))
-        audio_upconv4feature = self.audionet_upconvlayer4(torch.cat((audio_upconv3feature, audio_conv4feature), dim=1))
-        audio_upconv5feature = self.audionet_upconvlayer5(torch.cat((audio_upconv4feature, audio_conv3feature), dim=1))
-        audio_upconv6feature = self.audionet_upconvlayer6(torch.cat((audio_upconv5feature, audio_conv2feature), dim=1))
-        mask_prediction = self.audionet_upconvlayer7(torch.cat((audio_upconv6feature, audio_conv1feature), dim=1))
+        audio_upconv1feature = self.upconvlayer1(audio_conv7feature)
+        audio_upconv2feature = self.upconvlayer2(torch.cat((audio_upconv1feature, audio_conv6feature), dim=1))
+        audio_upconv3feature = self.upconvlayer3(torch.cat((audio_upconv2feature, audio_conv5feature), dim=1))
+        audio_upconv4feature = self.upconvlayer4(torch.cat((audio_upconv3feature, audio_conv4feature), dim=1))
+        audio_upconv5feature = self.upconvlayer5(torch.cat((audio_upconv4feature, audio_conv3feature), dim=1))
+        audio_upconv6feature = self.upconvlayer6(torch.cat((audio_upconv5feature, audio_conv2feature), dim=1))
+        mask_prediction = self.upconvlayer7(torch.cat((audio_upconv6feature, audio_conv1feature), dim=1))
         return mask_prediction
 
     @staticmethod
