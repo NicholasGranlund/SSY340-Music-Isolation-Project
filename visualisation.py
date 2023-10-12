7dc00b4408d4e64ea304ffbd469422f5e3e0bcee
Martino1129 <martin.ducros@imt-atlantique.net>
2023-10-12 16:11:51 +0200

    add docstring

--- visualisation.py
+++ visualisation.py
@@ -4,6 +4,11 @@ import os
 
 
 def create_fig_training(checkpoint_path, plot_name=''):
+    """
+    :param checkpoint_path: str path of the checkpoint .ckpt file
+    :param plot_name: (optional) name of the file must be .png default it's the checkpoint name
+    :return: (None): create the .png file with the plots
+    """
     checkpoint = torch.load(checkpoint_path)
     ax1 = plt.subplot(211)
     num_epochs = len(checkpoint['train_losses'])
